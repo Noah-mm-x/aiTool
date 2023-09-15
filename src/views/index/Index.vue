b5bcef54d423693f276d06683e8544488afc2dcb
孟凡旭 <15733225953@163.com>
2023-09-15 09:23:04 +0800

    feat: 项目

---
+++ src/views/index/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="index-container">
+        <div class="a">
+            aaaa
+            <div class="b">bbbb</div>
+        </div>
+        <a-space wrap>
+            <a-button type="primary">Primary Button</a-button>
+            <a-button>Default Button</a-button>
+            <a-button type="dashed">Dashed Button</a-button>
+            <a-button type="text">Text Button</a-button>
+            <a-button type="link">Link Button</a-button>
+        </a-space>
+        <div class="pt-[40px] bg-red-500 dark:bg-gray-800">sdfsdfss</div>
+        <div class="w-[100px] h-[100px] rounded-ful"></div>
+    </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.a {
+    background-color: red;
+    .b {
+        background-color: blue;
+    }
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    // 加上这行，tailwind才生效
+    content: ['./src/**/*.{vue,js,ts,jsx,tsx}', './src/**/**/*.{vue,js,ts,jsx,tsx}', './index.html'],
+    theme: {
+        extend: {
+            colors: {
+                active: {
+                    normal: '#333',
+                    dark: '#5F99F7',
+                },
+            },
+        },
+    },
+    plugins: ['./src/**/*.html', './src/**/*.vue', './src/**/**/*.vue', './*.html'],
+};
